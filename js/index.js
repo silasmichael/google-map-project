9f46b55b51c289d8cd7e66ff1c73185efafad8f7
Michael Silas <32719824+silasmichael@users.noreply.github.com>
2020-05-28 19:34:35 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function initMap() {
+    // Create the map.
+    const map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 7,
+        center: {lat: -6.7924, lng: 39.2083},
+    });
+}
